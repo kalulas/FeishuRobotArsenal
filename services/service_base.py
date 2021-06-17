77db5cef6866950cc4c60b67ebb11c8c03023d1a
edwardchen <edwardchen@123u.com>
2021-06-17 20:49:45 +0800

    feat(service): 实现roll服务部分逻辑

--- services/service_base.py
+++ services/service_base.py
@@ -9,6 +9,10 @@ class ServiceBase:
         self.bot = bot
         self.label = self.LABEL
         self.args = args
+        # 服务发起人
+        self.open_id = None
+        # 服务发起对话
+        self.open_chat_id = None
         self.on_init()
 
     def on_init(self, ):
@@ -16,17 +20,27 @@ class ServiceBase:
         子类自定义初始化操作
         """
 
-    def check_args(self, ) -> bool:
+    def check_args(self, raw_data:dict) -> bool:
         """
         检查命令参数是否合法
         """
+        if raw_data == None:
+            return False
+        
+        self.open_id = raw_data.get("open_id", "")
+        self.open_chat_id = raw_data.get("open_chat_id", "")
+        if self.open_id == "" or self.open_chat_id == "":
+            return False
+        
+        return True
 
     def invalid_args_process(self, ):
         """
         非法参数处理
         """
 
-    def process(self, raw_data:dict) -> bool:
+    def process(self, ) -> bool:
         """
         处理命令
         """
+        
--- services/service_roll.py
+++ services/service_roll.py
@@ -1,5 +1,6 @@
 from services.service_base import ServiceBase
 import json
+import re
 
 no_disturb_json = "no_disturb_users.json"
 
@@ -12,13 +13,45 @@ class ServiceRoll(ServiceBase):
         fp = open(no_disturb_json, 'r', encoding='utf-8')
         # 免打扰用户字典 {服务 -> {群组 -> [用户], }, }
         self.no_disturb_dict = json.load(fp)
+        # 服务发起人发送内容
+        self.text = ""
 
-    def check_args(self) -> bool:
-        # TODO 2
-        return super().check_args()
-
-    def process(self, raw_data: dict) -> bool:
-        if raw_data == None:
+    def check_args(self, raw_data:dict) -> bool:
+        if not super().check_args(raw_data):
             return False
-        # TODO 1
-        return super().process(raw_data)
+        
+        self.text = raw_data.get("text", "")
+        if self.text == "":
+            print("[service_roll] text is empty")
+            return False
+        
+        index = self.text.find(self.LABEL)
+        if index == -1:
+            print("[service_roll] label not found")
+            # message center 应该在查到 LABEL 后再实例化服务，这里是容错
+            return False
+
+        service_args = self.text[index:].split(' ')
+        if len(service_args) <= 1:
+            print("[service_roll] args not available")
+            self.invalid_args_process()
+            return False
+        
+        self.args = service_args[1:]
+        return True
+
+    def process(self, ) -> bool:
+        result = False
+        if re.match('\d+', self.args[0]):
+            result = self.__process_roll__()
+        return True
+
+    def __process_roll__(self,) -> bool:
+        print('[process roll] {0}'.format(self.args[0]))
+        members = self.bot.get_members_in_chat(self.open_chat_id)
+        if members is None:
+            # TODO log and error message
+            return False
+        
+        # TODO shuffle
+        return True
