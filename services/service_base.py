4641b35effe1efa9517d28a033cc9edd9d4edc0c
edwardchen <edwardchen@123u.com>
2021-06-18 16:39:43 +0800

    feat(service): 服务实现，容错处理与反馈

--- services/service_base.py
+++ services/service_base.py
@@ -1,13 +1,33 @@
 from robot_arsenal import RobotArsenal
+from enum import Enum
+import time
+
+class ServiceFailure(Enum):
+    ERROR_WRONG_ARGS = 0,
+    ERROR_NO_GROUP_FOUND = 1,
+    ERROR_USERNAME_NOT_FOUND = 2,
+    ERROR_UNKNOWN_USER_OR_GROUP = 3,
+    ERROR_UNEXPECTED = 999,
+
+class ServiceFailureMessage:
+    message_dict = {
+        ServiceFailure.ERROR_WRONG_ARGS : "命令参数错误！",
+        ServiceFailure.ERROR_NO_GROUP_FOUND : "未能找到群组",
+        ServiceFailure.ERROR_USERNAME_NOT_FOUND : "未能找到用户名",
+        ServiceFailure.ERROR_UNKNOWN_USER_OR_GROUP : "未知服务发起人/发起群组",
+        ServiceFailure.ERROR_UNEXPECTED : "未知错误",
+    }
+
 
 class ServiceBase:
     """
     机器人聊天服务基类
     """
     LABEL = "base"
+    LOG_FORMAT_STR = "[Service][{0}] {1}"
+
     def __init__(self, bot: RobotArsenal, args:list=[]):
         self.bot = bot
-        self.label = self.LABEL
         self.args = args
         # 服务发起人
         self.open_id = None
@@ -30,17 +50,30 @@ class ServiceBase:
         self.open_id = raw_data.get("open_id", "")
         self.open_chat_id = raw_data.get("open_chat_id", "")
         if self.open_id == "" or self.open_chat_id == "":
+            self.invalid_args_process(ServiceFailure.ERROR_UNKNOWN_USER_OR_GROUP)
             return False
         
         return True
 
-    def invalid_args_process(self, ):
+    def invalid_args_process(self, error_type: ServiceFailure, args: list=None) -> str:
         """
-        非法参数处理
+        非法参数处理，基类不做打印日志以外处理，返回错误信息
         """
+        error_message = self.get_error_message(error_type)
+        time_str = time.strftime("%Y-%m-%d %H:%M:%S", time.localtime())
+        log = self.LOG_FORMAT_STR.format(time_str, error_message)
+        print(log)
+        return error_message
 
-    def process(self, ) -> bool:
+    def process(self, raw_data:dict) -> bool:
         """
         处理命令
         """
-        
+        return self.check_args(raw_data)
+    
+    def get_error_message(self, error_type: ServiceFailure):
+        message = ServiceFailureMessage.message_dict[ServiceFailure.ERROR_UNEXPECTED]
+        if isinstance(error_type, ServiceFailure):
+            if error_type in ServiceFailureMessage.message_dict.keys():
+                message = ServiceFailureMessage.message_dict[error_type]
+        return message
--- services/service_roll.py
+++ services/service_roll.py
@@ -1,6 +1,7 @@
-from services.service_base import ServiceBase
+from services.service_base import ServiceBase, ServiceFailure
 import json
 import re
+import random
 
 no_disturb_json = "no_disturb_users.json"
 
@@ -9,12 +10,25 @@ class ServiceRoll(ServiceBase):
     机器人聊天服务:roll点
     """
     LABEL = "roll"
+    MESSAGE_NOT_GROUP_FOUND = "未找到 open_chat_id:{0} 对应群聊"
+    MESSAGE_ROLL_RESULT_TITLE = "由用户 <{0}> 发起的 <roll {1}> 结果如下"
+    MESSAGE_ROLL_ERROR_RESULT_TITLE = "发生了错误"
+    MESSAGE_ROLL_RESULT_BODY = "{0}\n(共{1}人)"
+
     def on_init(self):
         fp = open(no_disturb_json, 'r', encoding='utf-8')
         # 免打扰用户字典 {服务 -> {群组 -> [用户], }, }
         self.no_disturb_dict = json.load(fp)
         # 服务发起人发送内容
         self.text = ""
+
+    def invalid_args_process(self, error_type: ServiceFailure, args: list=None):
+        error_message = super().invalid_args_process(error_type, args)
+        if error_message == self.get_error_message(ServiceFailure.ERROR_UNKNOWN_USER_OR_GROUP):
+            return
+        
+        self.bot.send_rich_message_to_chat(self.open_chat_id, title=self.MESSAGE_ROLL_ERROR_RESULT_TITLE, content=error_message)
+        return
 
     def check_args(self, raw_data:dict) -> bool:
         if not super().check_args(raw_data):
@@ -32,26 +46,71 @@ class ServiceRoll(ServiceBase):
             return False
 
         service_args = self.text[index:].split(' ')
+        # 参数长度问题
         if len(service_args) <= 1:
-            print("[service_roll] args not available")
-            self.invalid_args_process()
+            self.invalid_args_process(ServiceFailure.ERROR_WRONG_ARGS)
             return False
         
         self.args = service_args[1:]
         return True
 
-    def process(self, ) -> bool:
+    def process(self, raw_data:dict) -> bool:
         result = False
-        if re.match('\d+', self.args[0]):
+        if not super().process(raw_data):
+            return result
+        # roll 人数 0不包含自己1包含自己
+        if self.args[0].isnumeric():
             result = self.__process_roll__()
-        return True
+        else:
+            self.invalid_args_process(ServiceFailure.ERROR_WRONG_ARGS)
+        return result
 
     def __process_roll__(self,) -> bool:
-        print('[process roll] {0}'.format(self.args[0]))
+        self.roll_result_size = (int)(self.args[0])
+        print('[process roll] {0}'.format(self.roll_result_size))
         members = self.bot.get_members_in_chat(self.open_chat_id)
         if members is None:
-            # TODO log and error message
+            self.invalid_args_process(ServiceFailure.ERROR_NO_GROUP_FOUND)
             return False
         
-        # TODO shuffle
+        random.shuffle(members)
+        member_names = []
+        idx = 0
+        
+        while len(member_names) < self.roll_result_size:
+            if idx >= len(members):
+                break
+            member_open_id = members[idx]["open_id"]
+            idx = idx + 1
+
+            # 
+            no_skip_self = len(self.args) >= 2 and self.args[1].isnumeric() and int(self.args[1]) == 1
+            # roll 到自己，跳过
+            if not no_skip_self and member_open_id == self.open_id:
+                continue
+            
+            # roll 到免打扰，跳过
+            if self.LABEL in self.no_disturb_dict.keys() and self.open_chat_id in self.no_disturb_dict[self.LABEL].keys():
+                if member_open_id in self.no_disturb_dict[self.LABEL][self.open_chat_id]:
+                    continue
+
+            user_name = self.bot.get_name_with_open_id(member_open_id)
+            if str(user_name) == "None":
+                self.invalid_args_process(ServiceFailure.ERROR_USERNAME_NOT_FOUND)
+                continue
+            member_names.append(user_name)
+            print(user_name)
+
+        at_members = ""
+        for member_name in member_names:
+            at_members = at_members + "@{0} ".format(member_name)
+
+        service_request_user = self.bot.get_name_with_open_id(self.open_id)
+        if str(service_request_user) == "None":
+                self.invalid_args_process(ServiceFailure.ERROR_USERNAME_NOT_FOUND)
+                return False
+        
+        send_message = self.MESSAGE_ROLL_RESULT_BODY.format(at_members, len(member_names))
+        send_title = self.MESSAGE_ROLL_RESULT_TITLE.format(service_request_user, self.roll_result_size)
+        self.bot.send_rich_message_to_chat(self.open_chat_id, title=send_title, content=send_message)
         return True
